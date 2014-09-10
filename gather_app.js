8c612420fbb317549ebd10ea7f47ed426ce4d140
GrahamBaskettIJKCO <graham.baskett@ijkco.com>
2014-09-10 15:40:55 -0700

    updated database model for new form fields

--- gather_app.js
+++ gather_app.js
@@ -18,13 +18,23 @@ app.use(bodyParser());
 var Emails = mongoose.model('Emails', {
 	name	: String,
 	e_mail	: String,
-	prof	: String,
-	SO		: String,
 	city	: String,
-	neighborhood : String,
+	hometown : String, 
+	industry : String,
+	SO		: String,
+	PI 		: String,
 	rstatus : String,
 	kids	: String,
-	pref	: String
+	pref	: String,
+	aHH		: String,
+	aDrinks	: String,
+	aDinner	: String,
+	aBrunch	: String,
+	aPark	: String,
+	aHike	: String,
+	aMusGal	: String,
+	aConcert : String,
+	aCoffee	: String
 });
 
 //routes
@@ -46,12 +56,23 @@ app.post('/api/emails', function(req,res) {
 		name	: req.body.name,
 		e_mail	: req.body.e_mail,
 		prof 	: req.body.prof,
-		SO		: req.body.SO,
 		city	: req.body.city,
-		neighborhood: req.body.neighborhood,
+		hometown : req.body.hometown,
+		industry : req.body.industry,
+		SO		: req.body.SO,
+		PI 		: req.body.PI,
 		rstatus : req.body.rstatus, 
 		kids	: req.body.kids,
 		pref	: req.body.pref,
+		aHH		: req.body.aHH,
+		aDrinks	: req.body.aDrinks,
+		aDinner	: req.body.aDinner,
+		aBrunch	: req.body.aBrunch,
+		aPark	: req.body.aPark,
+		aHike	: req.body.aHike,
+		aMusGal	: req.body.aMusGal,
+		aConcert : req.body.aConcert,
+		aCoffee : req.body.aCoffee,
 		done	: false
 	}, function(err,todo) {
 		if(err)
